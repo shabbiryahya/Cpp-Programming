7234ad63df6efef93bcb9d01b109732d102fc5e2
shabbiryahya <shabbirkatlariwala@gmail.com>
2023-09-25 12:11:16 +0530

    Modified:CharacterPrint from coder army 25-sep-2023 1st

--- characterPrint.cpp
+++ characterPrint.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     char row,col;
-    
+
     for(row='a';row<='e';row++)
     {
       for (col = 'a'; col <= 'e'; col++)
       {
        
-         cout<<col<<" ";
+         cout<<row<<" ";
       } 
       cout<<endl;
       
