b70b478c64b8f8a0c9b1f717640ebd1048d2447e
shabbiryahya <shabbirkatlariwala@gmail.com>
2023-09-25 12:07:40 +0530

    Added:CharacterPrint from coder army 25-sep-2023 1st

---
+++ characterPrint.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char row,col;
+    
+    for(row='a';row<='e';row++)
+    {
+      for (col = 'a'; col <= 'e'; col++)
+      {
+       
+         cout<<col<<" ";
+      } 
+      cout<<endl;
+      
+
+    }
+    return 0;
+}
