0dc38a9fd7a01d84711f34edd0e34920421cbb23
weconnectdirect <weconnectdirect@gmail.com>
2023-09-27 13:39:32 +0530

    Added:User input PatternPrinting2.cpp

--- patternPrint2.cpp
+++ patternPrint2.cpp
@@ -1,23 +1,32 @@
 #include<iostream>
 using namespace std;
-    //    1 2 3 4 5
-    //1   - - - - * 
-    //2   - - - * *
-    //3   - - * * *
-    //4   - * * * *
-    //5   * * * * *
+    //    1 2 3 4 5 6 7
+    //1        *
+    //2     * * *
+    //3     * * * *
+    //4     * * * * *
+    //5   * * * * * * *
 int main(int argc, char const *argv[])
 {
-    int row,col,n,m;
-    cout<<"Enter a Number";
-
-    cin>>m;
-
-    n=m-1;
-
-    for(row=1;row<=m;row++)
+    
+    int row,col,n=4;
+   
+    for(row=1;row<=5;row++)
     {
-        for(col=1;col<=m;col++)
+       
+        for(col=1;col<=5;col++)
+        {
+             if(col<=n)
+             {
+                  cout<<"  ";
+             } 
+             else
+             {
+                  cout<<"* ";
+             }
+        }
+
+        for(col=4;col>=1;col--)
         {
              if(col<=n)
              {
@@ -27,11 +36,16 @@ int main(int argc, char const *argv[])
              else
              {
                   cout<<"* ";
+                  
                
                                 
              }
         }
+        
         n--;
+      
+      
+        
         cout<<endl;
     }
     return 0;
